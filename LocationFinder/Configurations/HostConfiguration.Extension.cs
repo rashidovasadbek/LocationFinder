2912881cb6e7a211efcd093e5fc2392232b65769
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-25 22:36:25 +0500

    Added HostConfiguration

--- LocationFinder/Configurations/HostConfiguration.Extension.cs
+++ LocationFinder/Configurations/HostConfiguration.Extension.cs
@@ -1,6 +1,77 @@
-﻿namespace LocationFinder.Configurations;
+﻿using LocationFinder.Models;
+using LocationFinder.Service;
+using LocationFinder.Settings;
+using Microsoft.Extensions.Http;
 
-public class HostConfiguration_Extension
+namespace LocationFinder.Configurations;
+
+public static partial class HostConfiguration
 {
+    /// <summary>
+    /// register data access foundation services
+    /// </summary>
+    /// <param name="builder">Application builder</param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddLocationInfrastructure(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ILocationFinderService, LocationFinderService>();
+        builder.Services.AddHttpClient();
+        
+        builder.Services.Configure<LocationFinderSettings>(
+            builder.Configuration.GetSection(nameof(LocationFinderSettings)));
+        
+        return builder;
+    }
     
+    /// <summary>
+    /// Configures exposers including controllers
+    /// </summary>
+    /// <param name="builder">Application builder</param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers();
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures devTools including controllers
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <returns></returns>
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+    
+
+    /// <summary>
+    /// Add Controller middleWhere
+    /// </summary>
+    /// <param name="app">Application host</param>
+    /// <returns>Application host</returns>
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+
+    /// <summary>
+    /// Add Controller middleWhere
+    /// </summary>
+    /// <param name="app">Application host</param>
+    /// <returns>Application host</returns>
+    private static WebApplication UseDevTools(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
 }
--- LocationFinder/Configurations/HostConfiguration.cs
+++ LocationFinder/Configurations/HostConfiguration.cs
@@ -1,6 +1,18 @@
 ﻿namespace LocationFinder.Configurations;
 
-public class HostConfiguration
+public static partial class HostConfiguration
 {
-    
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder.AddExposers().AddDevTools().AddLocationInfrastructure();
+
+        return new();
+    }
+
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app.UseExposers().UseDevTools();
+
+        return new();
+    }
 }
