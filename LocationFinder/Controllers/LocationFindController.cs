e3fed018cacc8943aa372370541e590cf3390595
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-12-25 22:36:47 +0500

    Added LocationFinderService

--- LocationFinder/Controllers/LocationFindController.cs
+++ LocationFinder/Controllers/LocationFindController.cs
@@ -1,6 +1,23 @@
-﻿namespace LocationFinder.Controllers;
+﻿using System.Security.Cryptography;
+using LocationFinder.Models;
+using LocationFinder.Service;
+using LocationFinder.Settings;
+using Microsoft.AspNetCore.Mvc;
 
-public class LocationFindController
+namespace LocationFinder.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LocationFindController(ILocationFinderService locationFinderService) : ControllerBase
 {
-    
+    [HttpGet]
+    public async ValueTask<IActionResult> FindLocationAsync(
+        [FromQuery] decimal latitude,
+        [FromQuery] decimal longitude)
+    {
+        var result =
+            await locationFinderService.FindAsync(latitude, longitude);
+   
+        return Ok(result);
+    }
 }
